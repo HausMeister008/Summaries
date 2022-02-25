349a4213b8bbea24ea99ddd94bc3199298611bf6
Timo Stolz <timo.stolz@gmail.com>
2022-02-25 14:20:56 +0100

    add rollup build settings

---
+++ backend/rollup.config.js
@@ -0,0 +1,32 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { dependencies } from './package.json';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+
+export default {
+  input: 'src/app.ts',
+  external: ['pg-native'],
+  output: {
+    sourcemap: true,
+    manualChunks: {
+      vendor: Object.keys(dependencies),
+    },
+    dir: 'dist',
+    format: 'cjs',
+    entryFileNames: '[name].js', // '[name]-[hash].js',
+    chunkFileNames: '[name].js' // '[name]-[hash].js'
+  },
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json',
+    }),
+    json(),
+    nodeResolve({
+      preferBuiltins: true
+    }),
+    commonjs({
+      ignore: ['pg-native' , './native']
+    })
+  ]
+};
